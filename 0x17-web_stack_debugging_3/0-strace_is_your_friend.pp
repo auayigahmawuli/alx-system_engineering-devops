bf7881b2905e067d04a6338e0999ab9502236adf
Prosper Mawuli Auayigah <auayigahma@outlook.com>
2023-11-09 19:34:05 -0800

    webstack-deb

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,22 +1,6 @@
-# 0-strace_is_your_friend.pp
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-# Ensure the required configuration file exists
-file { '/etc/apache2/some_config_file.conf':
-  ensure => file,
-  owner  => 'www-data', # Adjust the owner and permissions as needed
-  group  => 'www-data',
-  mode   => '0644',
-  source => 'puppet:///modules/my_module/some_config_file.conf',
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
-
-# Notify the Apache service to reload when the file changes
-service { 'apache2':
-  ensure    => running,
-  subscribe => File['/etc/apache2/some_config_file.conf'],
-}
-
-
-
-
-
-
